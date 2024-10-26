39b2664b34102ccdff51dd70fe8e7a6dd4bb4ad5
Meyer <meyer@attal.dev>
2024-10-26 21:36:51 +0200

    scalingo

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 if [ "$APP_TO_BUILD" = "api" ] ; then
   exec node apps/api/build/bin/server.js
 else
-  exec PORT=$PORT node apps/$APP_TO_BUILD/server.js
+  # Ensure PORT is set, use a default if not provided
+  PORT=${PORT}
+  echo "Starting $APP_TO_BUILD on port $PORT"
+  exec node /$BUILD_DIR/apps/$APP_TO_BUILD/server.js
 fi
